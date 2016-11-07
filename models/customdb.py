46c4e4d32e0617558dd8fed7ff29360ab4b723e1
shadeyshades <shades@initialhit.net>
2016-11-07 17:21:09 +0000

    initial

---
+++ models/customdb.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+#db.define_table('audit', Field('username', type='string'), Field('command',type='string'), Field('results_location',type='string'), Field('description',type='string'), Field('justification',type='string'), Field('timestamp',type='string'))
---
+++ modules/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# This is written to test my other scripts from to replicate web2py
+'''
+from nmapscanner import *
+
+try:
+    superobject = SuperScan()
+    results = superobject.standardscan("10.1.10.0/24", '-n --script nbstat.nse -O  -T 5')
+    print(results)
+except Exception as e:
+    print("%s" % e)
+print("Done!")
+'''
