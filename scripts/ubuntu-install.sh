45b78c520e3af42879da649d08be29a13f8f46d6
shadeyshades <abjustinbowen@yahoo.com>
2016-11-30 00:05:25 -0800

    Install script added, renamed external to scripts.

--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -11,7 +11,12 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-apt-get install python-pip -yf
+apt-get install python-pip wget unzip -yf
+wget http://www.web2py.com/examples/static/web2py_src.zip --directory-prefix=/opt
+unzip /opt/web2py_src.zip
+mkdir -P /opt/web2py/applications/SimpleAssetInventory
+cp -rfu ../* /opt/web2py/applications/SimpleAssetInventory
+chown $USER:$USER /opt/web2py -R
 pip install -r requirements.txt
 cp -fu ./cronrun.py /etc/cron.d/
 (crontab -l ; echo "*/1 * * * * /etc/cron.d/cronrun.py")  | uniq - | crontab -
