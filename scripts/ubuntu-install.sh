dbf504c61fa28114824cd9c1350f62eb9889fecf
Justin B <abjustinbowen@yahoo.com>
2017-03-15 11:03:01 -0500

    Update ubuntu-install.sh
    Modify to rely on ansible.

--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -11,35 +11,12 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-chown $USER:$USER ../../ -R
-mkdir -P /opt/web2py/applications/SimpleAssetInventory
-if [ $1 == "offline"]; then
-    # Use local cached version... may or may not work... not ideal
-    unzip ./external/*.zip
-    tar -xvzf ./External/*.tar.gz
-    dpkg -i ./External/nmap_6.40-0.2ubuntu1_amd64.deb
-    cp -fu ./External/web2py /opt/
-    python ./External/python-libnmap-master/setup.py install
-    python ./External/python-nmap-0.6.1/setup.py install
-else
-    # Pull the latest and greatest of everything directly from our friends online
-    apt-get install python-pip wget unzip -yf
-    rm -rf /opt/web2py*
-    wget http://www.web2py.com/examples/static/web2py_src.zip --directory-prefix=/opt 
-    pip install -r requirements.txt
-    unzip /opt/web2py_src.zip -d /opt/
-fi
-# Now to move in to a nice little setup home
-mkdir -P /opt/web2py/applications/SimpleAssetInventory
-cp -rfu ../* /opt/web2py/applications/SimpleAssetInventory
-# Sell the farm to web server, but make sure we are in the in crowd
-usermod $USER -a -G www-data
-newgrp www-data
-chown www-data:www-data /opt/web2py/ -R
-rm -rf /home/www-data/web2py/applications
-mkdir -P /home/www-data/web2py/
-sudo -u www-data ln -s ../applications/ /home/www-data/web2py/applications
-# Put our main man at the head of the agency
-cp -fu ./cronrun.py /etc/cron.d/
-# Update cron to check on our main man every minutes... he has problems now
-(crontab -l ; echo "*/1 * * * * /etc/cron.d/cronrun.py")  | uniq - | crontab -
+apt-get update
+apt-get install python-pip ansible -y
+pip install pip --upgrade
+pip install ansible --upgrade
+mkdir -p /etc/ansible
+cp -fu ../conf/hosts /etc/ansible
+chmod 755 /etc/ansible/hosts
+ansible-playbook local -c local install-pyAssetContext.yml
+
